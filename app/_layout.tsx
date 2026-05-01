5d871e092b65a5803c5ac3f331e21d3509e2795c
victor <thysvic@gmail.com>
2026-05-01 12:42:36 +0200

    Style(font): added custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Stack } from 'expo-router';
 import { TamaguiProvider } from 'tamagui';
 
@@ -6,8 +7,34 @@ import tamaConfig from '@/tamagui.config';
 export const unstable_settings = {
   anchor: '(tabs)',
 };
+=======
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import tamaConfig from "@/tamagui.config";
+import { Stack } from "expo-router";
+import { TamaguiProvider } from "tamagui";
+>>>>>>> Stashed changes
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    Satoshi: require("../assets/fonts/Satoshi-Regular.otf"),
+    SatoshiLight: require("../assets/fonts/Satoshi-Light.otf"),
+    SatoshiMedium: require("../assets/fonts/Satoshi-Medium.otf"),
+    SatoshiBold: require("../assets/fonts/Satoshi-Bold.otf"),
+    SatoshiBlack: require("../assets/fonts/Satoshi-Black.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) return null;
+
   return (
     <TamaguiProvider config={tamaConfig}>
       <Stack>
