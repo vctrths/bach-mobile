0aa46161eef8ea61100a313944eb36e85e7f5ec9
victor <thysvic@gmail.com>
2026-05-21 15:34:20 +0200

    fix: mock react-native-maps as empty module on web via metro resolveRequest

--- metro.config.js
+++ metro.config.js
@@ -9,10 +9,22 @@ module.exports = (() => {
     ...transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
   };
+
+  const defaultResolveRequest = resolver.resolveRequest;
+
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
     sourceExts: [...resolver.sourceExts, "svg"],
+    resolveRequest: (context, moduleName, platform) => {
+      if (moduleName === "react-native-maps" && platform === "web") {
+        return { type: "empty" };
+      }
+      if (typeof defaultResolveRequest === "function") {
+        return defaultResolveRequest(context, moduleName, platform);
+      }
+      return context.resolveRequest(context, moduleName, platform);
+    },
   };
 
   return config;
