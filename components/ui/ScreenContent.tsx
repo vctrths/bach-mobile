5f983899c31a0e342fe4ed7bf18f07f99aaf80d4
victor <thysvic@gmail.com>
2026-05-29 14:34:22 +0200

    fix: remove flex height constraint on ScreenContent to restore scrolling

--- components/ui/ScreenContent.tsx
+++ components/ui/ScreenContent.tsx
@@ -1,9 +1,7 @@
 import { styled, YStack } from "tamagui";
 
 export default styled(YStack, {
-  flex: 1,
   paddingHorizontal: "$4",
   paddingVertical: "$6",
   gap: "$6",
-  paddingBottom: "$25",
 });
